d8e73e449e7030e83f8f5a42e83c4c100ef99316
LeonidGHA <stowk1992@gmail.com>
2022-07-06 17:06:59 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,5 @@
 const userInputEl = document.querySelector("#validation-input");
 
-// userInputEl.addEventListener("blur", onValidInput);
-
-// const onValidInput = function () {
-//   if (
-//     userInputEl.value.length === Number(userInputEl.dataset.id) ||
-//     userInputEl.value.length >= Number(userInputEl.dataset.id)
-//   ) {
-//     userInputEl.classList.add("valid");
-//   } else userInputEl.classList.add("invalid");
-// };
-
 userInputEl.addEventListener("blur", () => {
   if (userInputEl.value.length === Number(userInputEl.dataset.length)) {
     userInputEl.classList.add("valid");
