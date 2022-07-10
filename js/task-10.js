2101e7e83d7f1a9e2ec08690dd95ac62d97c5221
LeonidGHA <stowk1992@gmail.com>
2022-07-10 17:35:39 +0300

    add commit

--- js/task-10.js
+++ js/task-10.js
@@ -33,23 +33,23 @@ function destroyBoxes() {
 createBtn.addEventListener("click", createBoxes);
 destroyBtn.addEventListener("click", destroyBoxes);
 
-// function createBoxes(amount) {
-//   const fragment = document.createDocumentFragment();
-//   for (let i = 1; i <= amount; i += 1) {
-//     const size = 30 + i * 10;
-//     const newBox = document.createElement("div");
-//     newBox.style.width = size + "px";
-//     newBox.style.height = size + "px";
-//     newBox.style.backgroundColor = getRandomHexColor();
-//     newBox.style.marginRight = "20px";
-//     fragment.append(newBox);
-//   }
-//   newBoxes.append(fragment);
-// }
-// function destroyBoxes() {
-//   newBoxes.innerHTML = "";
-// }
-// createBtn.addEventListener("click", () =>
-//   createBoxes(Number(userInputNumber.value))
-// );
-// destroyBtn.addEventListener("click", destroyBoxes);
+function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i += 1) {
+    const size = 30 + i * 10;
+    const newBox = document.createElement("div");
+    newBox.style.width = size + "px";
+    newBox.style.height = size + "px";
+    newBox.style.backgroundColor = getRandomHexColor();
+    newBox.style.marginRight = "20px";
+    fragment.append(newBox);
+  }
+  newBoxes.append(fragment);
+}
+function destroyBoxes() {
+  newBoxes.innerHTML = "";
+}
+createBtn.addEventListener("click", () =>
+  createBoxes(Number(userInputNumber.value))
+);
+destroyBtn.addEventListener("click", destroyBoxes);
